531332aebd91996735fdf24de1c83b40f7fd9f2b
vaishnavi modem <31727530+vaishnavimodem2215@users.noreply.github.com>
2025-11-25 21:19:30 +0530

    Assignment Day1

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+let Fullname =  "Harika";
+console.log(Fullname, typeof Fullname);
+
+//Company Name
+let CompanyName = "Fiserv";
+console.log(CompanyName, typeof CompanyName);
+
+//Phone Number
+let PhoneNumber = 9085937680;
+console.log(PhoneNumber, typeof PhoneNumber);
+
+let Test =  true;
+console.log(Test, typeof Test);
+
+let isAutomation = null;
+console.log(isAutomation, typeof isAutomation);
+
+let hasPlaywright;
+console.log(hasPlaywright,typeof hasPlaywright);
